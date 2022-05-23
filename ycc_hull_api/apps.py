72616fe5dcf0e8ef864c01e02f5404197b447148
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2022-05-23 19:39:30 +0200

    Remove Django

--- ycc_hull_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class YccHullApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ycc_hull_api'
--- ycc_hull_api/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse, HttpRequest
-
-
-def index(request: HttpRequest):
-    return HttpResponse("Hello, world")
