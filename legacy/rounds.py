8e06fa34ec7784deb41ba9eda1b1230afa886611
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2022-05-23 20:13:51 +0200

    Format

--- legacy/rounds.py
+++ legacy/rounds.py
@@ -8,13 +8,13 @@ def pack_to_bytes(data):
     if isinstance(data, int):
         # Perl pack('N', ...) is 32-bit big-endian (https://perldoc.perl.org/functions/pack)
         # It translates to `>I` in Python struct.pack (https://docs.python.org/3/library/struct.html#format-strings)
-        return base64.b64encode(struct.pack('>I', data))
+        return base64.b64encode(struct.pack(">I", data))
     else:
         raise ValueError(f"Unsupported value type: " + type(data))
 
 
 def pack_to_string(data):
-    return pack_to_bytes(data).decode('ascii')
+    return pack_to_bytes(data).decode("ascii")
 
 
 print(pack_to_bytes(rounds))
