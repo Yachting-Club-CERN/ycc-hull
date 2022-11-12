36a39f58e0c02e46402d855d6fe62f46aabd9a98
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2022-11-12 12:20:41 +0100

    Update test db

--- db/init-local/2-create-schema.sh
+++ db/init-local/2-create-schema.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
 
 # In this way it produces a container start failure upon SQL error
-sqlplus -s YCCLOCAL/changeit@XE @"$SCRIPT_DIR/sql/schema-local.sql"
+sqlplus -s YCCLOCAL/changeit@XE @"$SCRIPT_DIR/sql/schema-local.sql.noautorun"
