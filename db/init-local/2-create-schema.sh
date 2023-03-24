b66dc7e7b0283cf1f8ee11735469a43dce0c17dd
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-03-24 01:13:45 +0100

    Move Oracle DB-related files to ycc-infra

--- db/init-local/2-create-schema.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
-
-# In this way it produces a container start failure upon SQL error
-sqlplus -s YCCLOCAL/changeit@XE @"$SCRIPT_DIR/sql/schema-local.sql.noautorun"
