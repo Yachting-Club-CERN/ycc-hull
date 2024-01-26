23661412b320cad42f48cd667b1b1ca942993a12
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2024-01-27 00:44:44 +0100

    Add load tests (#56)
    Closes #54

---
+++ load_tests/load_test_auth_utils.py
@@ -0,0 +1,39 @@
+"""
+Authentication utilities for load tests.
+"""
+import requests
+
+from load_tests.load_test_config import AUTH_CLIENT_ID, AUTH_REALM, AUTH_URL
+
+
+def create_auth_header(access_token: str) -> dict:
+    return {"Authorization": f"Bearer {access_token}"}
+
+
+def get_access_token(user: str) -> str:
+    response = requests.post(
+        f"{AUTH_URL}/realms/{AUTH_REALM}/protocol/openid-connect/token",
+        data={
+            "grant_type": "password",
+            "client_id": AUTH_CLIENT_ID,
+            "username": user,
+            "password": user,
+            "scope": "openid profile email",
+        },
+        timeout=30,
+    )
+    assert response.status_code == 200, response
+
+    return response.json()["access_token"]
+
+
+def get_user_id(access_token: str) -> int:
+    response = requests.post(
+        f"{AUTH_URL}/realms/{AUTH_REALM}/protocol/openid-connect/userinfo",
+        headers=create_auth_header(access_token),
+        timeout=30,
+    )
+    assert response.status_code == 200, response
+
+    # E.g., f:a9b693ac-d9aa-43c7-8b68-b3bb7d30cc8e:1
+    return int(response.json()["sub"].split(":")[2])
---
+++ load_tests/load_test_helpers.py
@@ -0,0 +1,58 @@
+"""
+Helpers API load tests.
+"""
+import random
+from locust import HttpUser, task
+
+from load_tests.load_test_auth_utils import (
+    get_access_token,
+    get_user_id,
+)
+from load_tests.load_test_config import (
+    ADMIN_USER,
+    OTHER_USER,
+)
+from load_tests.load_test_helpers_utils import (
+    create_task,
+    get_task,
+    get_tasks,
+    sign_up_as_captain,
+    sign_up_as_helper,
+)
+
+
+ADMIN_ACCESS_TOKEN = get_access_token(ADMIN_USER)
+ADMIN_ID = get_user_id(ADMIN_ACCESS_TOKEN)
+OTHER_ACCESS_TOKEN = get_access_token(OTHER_USER)
+OTHER_ID = get_user_id(OTHER_ACCESS_TOKEN)
+
+
+def get_random_access_token() -> str:
+    return random.choice([ADMIN_ACCESS_TOKEN, OTHER_ACCESS_TOKEN])
+
+
+class HelpersLoadTest(HttpUser):
+    """
+    Helpers API load test.
+    """
+
+    @task
+    def list_tasks(self) -> None:
+        get_tasks(self.client, get_random_access_token())
+
+    @task
+    def list_and_get_task(self) -> None:
+        access_token = get_random_access_token()
+        tasks = get_tasks(self.client, access_token)
+
+        get_task(self.client, random.choice(tasks)["id"], access_token)
+
+    @task
+    def create_and_get_task(self) -> None:
+        create_task(self.client, ADMIN_ID, ADMIN_ACCESS_TOKEN)
+
+    @task
+    def create_and_get_task_and_sign_up(self) -> None:
+        task_ = create_task(self.client, ADMIN_ID, ADMIN_ACCESS_TOKEN)
+        sign_up_as_captain(self.client, task_["id"], ADMIN_ACCESS_TOKEN)
+        sign_up_as_helper(self.client, task_["id"], OTHER_ACCESS_TOKEN)
