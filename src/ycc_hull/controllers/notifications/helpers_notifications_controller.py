21e6422b01dcf6f63472cb03355d6b16a7cca6f7
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2025-03-23 14:07:26 +0100

    oops

--- src/ycc_hull/controllers/notifications/helpers_notifications_controller.py
+++ src/ycc_hull/controllers/notifications/helpers_notifications_controller.py
@@ -169,7 +169,7 @@ class HelpersNotificationsController(BaseController):
     <li>Validate the tasks. During validation you will be asked to mark which members showed up and optionally you can leave a comment</li>
     <li>If the task was not done before the deadline, maybe you want to extend it.</li>
 </ul>
-"""mo
+"""
             )
         ).build()
 
